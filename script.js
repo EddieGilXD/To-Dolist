173101d325dc16299bb00d950afd7f32289015b4
EddieGilXD <djuntaied@gmail.com>
2024-05-29 20:57:30 -0600

    refactorizando codigo
    trabajando js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-class tarea (nombreTarea) {
-    this.nombreTarea = tarea;
-    
+document.querySelector('#push').onclick = function (){
+    if (document.querySelector('#newTask input').value.length == 0){
+        alert("Ingrese la tarea de nuevo");
+    }
 }
